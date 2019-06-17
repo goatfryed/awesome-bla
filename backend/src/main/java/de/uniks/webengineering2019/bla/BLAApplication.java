40a3a35ae258ea8a8370980b08ce194d87306053
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-17 18:35:00 +0200

    added spring initializer boilerplate for java backend

---
+++ backend/src/main/java/de/uniks/webengineering2019/bla/BLAApplication.java
@@ -0,0 +1,13 @@
+package de.uniks.webengineering2019.bla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BLAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BLAApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/uniks/webengineering2019/bla/BLAApplicationTests.java
@@ -0,0 +1,16 @@
+package de.uniks.webengineering2019.bla;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BLAApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
