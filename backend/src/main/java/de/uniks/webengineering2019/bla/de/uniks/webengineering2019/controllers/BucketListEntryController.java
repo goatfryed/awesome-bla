d7fe67d0fa96b61e263132a4c4144c3b3ee29b4a
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-24 20:49:29 +0200

    added /bucketlists/{id}/entries/ to list all entries of an bucketlist. currently returning all all.

---
+++ backend/src/main/java/de/uniks/webengineering2019/bla/de/uniks/webengineering2019/controllers/BucketListEntryController.java
@@ -0,0 +1,30 @@
+package de.uniks.webengineering2019.bla.de.uniks.webengineering2019.controllers;
+
+import de.uniks.webengineering2019.bla.de.uniks.webengineering2019.model.BucketListEntry;
+import de.uniks.webengineering2019.bla.de.uniks.webengineering2019.repositories.BucketListEntryRepository;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@CrossOrigin
+@RequestMapping("/bucketlists/{id}/entries")
+@RestController
+public class BucketListEntryController {
+
+    @NonNull
+    private final BucketListEntryRepository entryRepository;
+
+    public BucketListEntryController(BucketListEntryRepository entryRepository) {
+        this.entryRepository = entryRepository;
+    }
+
+    @GetMapping("/")
+    public List<BucketListEntry> list()
+    {
+        return entryRepository.findAll();
+    }
+}
--- backend/src/main/java/de/uniks/webengineering2019/bla/de/uniks/webengineering2019/repositories/BucketListEntryRepository.java
+++ backend/src/main/java/de/uniks/webengineering2019/bla/de/uniks/webengineering2019/repositories/BucketListEntryRepository.java
@@ -3,5 +3,9 @@ package de.uniks.webengineering2019.bla.de.uniks.webengineering2019.repositories
 import de.uniks.webengineering2019.bla.de.uniks.webengineering2019.model.BucketListEntry;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BucketListEntryRepository extends CrudRepository<BucketListEntry, Long> {
+
+    List<BucketListEntry> findAll();
 }
