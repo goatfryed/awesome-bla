4323a94a0909877e00c0ad5d6f94aef3a8b6b3bf
tost <tost@tost-soft.de>
2019-06-24 16:43:42 +0200

    fixed build frontend container

--- deploy/build_frotend.sh
+++ deploy/build_frotend.sh
@@ -2,4 +2,4 @@ docker run -it --rm --name web-engenering-build-frontend \
   -v /home/lukas/workspace/awesome-bucket-list/frontend:/proj \
   -w /proj \
   node:8.16	 \
-  npm run build
+  bash -c 'npm install && npm run build'
