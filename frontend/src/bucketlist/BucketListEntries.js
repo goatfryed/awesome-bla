8707a363e899832cc101b6c465c9631af56f942a
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-24 21:51:50 +0200

    fetch entries to console

--- frontend/src/bucketlist/BucketListEntries.js
+++ frontend/src/bucketlist/BucketListEntries.js
@@ -1,5 +1,15 @@
-import * as React from "react";
+import React, {useEffect, useState} from "react";
+import {backendUrl} from "../config";
 
 export function BucketListEntries() {
+    useEffect(() => {
+            (async () => {
+                const response = await fetch( backendUrl + "/bucketlists/1/entries/");
+                const json = await response.json();
+                console.log(json);
+            })();
+        },
+        []
+    );
     return <span>Under construction</span>;
 }
