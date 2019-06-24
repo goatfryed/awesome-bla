4d8a0e910ffca1e83896931b1ea24dbe55a59815
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-06-24 20:19:21 +0200

    created BucketListEntryRepository

---
+++ backend/src/main/java/de/uniks/webengineering2019/bla/de/uniks/webengineering2019/repositories/BucketListEntryRepository.java
@@ -0,0 +1,7 @@
+package de.uniks.webengineering2019.bla.de.uniks.webengineering2019.repositories;
+
+import de.uniks.webengineering2019.bla.de.uniks.webengineering2019.model.BucketListEntry;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BucketListEntryRepository extends CrudRepository<BucketListEntry, Long> {
+}
