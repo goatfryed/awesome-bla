5060e6ec7be5ef1f0e024db704d8772a970f1b21
Lukas <hagenaul@fh-trier.de>
2019-06-29 15:50:48 +0200

    removed by accident added packege lock file

--- frontend/src/Configuration.jsx
+++ frontend/src/Configuration.jsx
@@ -1,4 +1,4 @@
 import React from "react";
 
-const backend = process.env.NODE_ENV === 'production' ?  "https://bucketlsit.tost-soft.de" : "http://localhost:8080";
+const backend = process.env.NODE_ENV === 'production' ?  "https://bucketlist.tost-soft.de" : "http://localhost:8080";
 export default backend;
