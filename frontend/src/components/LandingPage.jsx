0d76f1e593aff0f4b5ebf6097202f00a6d798eb9
Michael Prasil <michaprx@gmail.com>
2019-07-01 16:58:50 +0200

    Created a nav bar with title and added materialized.

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const LandingPage = () => {
-	return (
-		<span>What do you want to do before you die?</span>
-	);
+class LandingPage extends Component {
+	render() {
+		const bucketLists = null;
+
+		return (
+			<div>
+				<nav className="nav-wrapper grey darken-3">
+					<div className="container">
+						<div className="brand-logo white-text">Awesome Bucket List</div>
+					</div>
+				</nav>
+				<div className="container white">
+					{ bucketLists }
+				</div>
+			</div>
+		);
+	}
 }
 
 export default LandingPage;
