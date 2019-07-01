92cdfa0889540b9a860d1c47af3d71b28619e522
Michael Prasil <michaprx@gmail.com>
2019-07-01 17:35:57 +0200

    Layoutet the collection of lists.

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 
 class LandingPage extends Component {
 	render() {
-		const bucketLists = null;
+		const bucketLists = (
+			<div>
+				<div className="collection-item grey lighten-3">Alpha</div>
+				<div className="collection-item grey lighten-3">Beta</div>
+				<div className="collection-item grey lighten-3">Gamma</div>
+				<div className="collection-item grey lighten-3">Delta</div>
+			</div>
+		);
 
 		return (
-			<div className="container white">
-				{ bucketLists }
+			<div className="container left">
+				<h5>Bucket Lists</h5>
+				<div className="collection grey lighten-1">
+					{ bucketLists }
+				</div>
 			</div>
 		);
 	}
