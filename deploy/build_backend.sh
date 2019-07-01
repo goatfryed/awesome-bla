054d5b69988f4cfd164f021a455cd4ee62d06036
Lukas Hagenauer <tost@tost-soft.de>
2019-07-01 16:04:26 +0200

    fixed build container for new frontend from devlelop

--- deploy/build_backend.sh
+++ deploy/build_backend.sh
@@ -1,5 +1,6 @@
 docker run -it --rm --name web-engenering-build-backend \
   -v /home/lukas/workspace/awesome-bucket-list/backend:/proj \
   -w /proj \
+  -u $(id -u ${USER}):$(id -g ${USER}) \
   gradle:5.4-jdk8 \
   gradle build -x test
--- deploy/build_frotend.sh
+++ deploy/build_frotend.sh
@@ -1,5 +1,6 @@
 docker run -it --rm --name web-engenering-build-frontend \
   -v /home/lukas/workspace/awesome-bucket-list/frontend:/proj \
   -w /proj \
+  -u $(id -u ${USER}):$(id -g ${USER}) \
   node:8.16	 \
   bash -c 'npm install && npm run build'
