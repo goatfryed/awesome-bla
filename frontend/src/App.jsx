6592562e5c9342563e83eaa8728eeb80200a0cfd
Michael Prasil <michaprx@gmail.com>
2019-07-01 15:36:48 +0200

    Adjusted package.json and app.jsx.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
+import Page404 from './components/Page404';
+import LandingPage from './components/LandingPage';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Switch>
-				<Route path="/" exact component={LandingPage}/>
-				<Route path="/" component={Page404}/>
+				<Route path="/" exact component={LandingPage} />
+				<Route path="/" component={Page404} />
 			</Switch>
 		</BrowserRouter>
 	);
