4cd9e360029b2c0c907d83793f58b75367ce31bb
Maggi64 <maximilian@dewaldnet.de>
2019-07-06 17:19:41 +0200

    switch to bulma

--- frontend/src/components/basic-components/Navbar.js
+++ frontend/src/components/basic-components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Authentication from "../../authentication/Authentication";
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 export class Navbar extends React.Component {
   //Authentification
@@ -23,35 +23,65 @@ export class Navbar extends React.Component {
   //------------------
   render() {
     return (
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#!" class="brand-logo center">
-            Logo
+      <nav class="navbar has-shadow" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
           </a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-            <li>
-              <NavLink exact to="/" activeClassName="activeLink">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/friends" activeClassName="activeLink">Friends</NavLink>
-            </li>
-            {/* Authentication */}
-            {Authentication.isAuthenticated() && (
-              <li>
-                <a onClick={Authentication.logout}>Logout</a>
-              </li>
-            )}
-            {!Authentication.isAuthenticated() && (
-              <li>
-                <a href={this.state.url}>Login</a>
-              </li>
-            )}
-            {Authentication.isAuthenticated() && (
-              <li>
-                <a className="user-info">{Authentication.getUser().sub}</a>
-              </li>
-            )}
-          </ul>
+
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <span class="navbar-item">
+              <NavLink exact to="/" activeClassName="activeLink">
+                Home
+              </NavLink>
+            </span>
+            <span class="navbar-item">
+              <NavLink to="/friends" activeClassName="activeLink">
+                Friends
+              </NavLink>
+            </span>
+            <span class="navbar-item">
+              <NavLink to="/listentry/new" activeClassName="activeLink">
+                [WIP] ListEntryNew
+              </NavLink>
+            </span>
+            <span class="navbar-item">
+              <NavLink to="/bucketlists/all" activeClassName="activeLink">
+                [WIP] DisplayAllBucketLists
+              </NavLink>
+            </span>
+          </div>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              {/* Authentication */}
+              {Authentication.isAuthenticated() && (
+                <a class="button is-primary" onClick={Authentication.logout}>
+                  <strong>Logout</strong>
+                </a>
+              )}
+              {!Authentication.isAuthenticated() && (
+                <a class="button is-primary" href={this.state.url}>
+                  <strong>Login</strong>
+                </a>
+              )}
+              {Authentication.isAuthenticated() && (
+                <a class="button is-light">
+                  <strong>{Authentication.getUser().sub}</strong>
+                </a>
+              )}
+            </div>
+          </div>
         </div>
       </nav>
     );
