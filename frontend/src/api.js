9b9d67feb283945c488004a7cde01ef738128644
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-06 17:57:46 +0200

    backendFetch for default json fetch requests to the backendUrl

---
+++ frontend/src/api.js
@@ -0,0 +1,30 @@
+import {backendUrl} from "./config";
+
+
+const defaultHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+};
+
+export async function backendFetch(url, config) {
+
+    const configHeaders = (config && config.headers) || {};
+
+    const response = await fetch(
+        backendUrl + url,
+        {
+            ...config,
+            headers: {
+                ...defaultHeaders,
+                ...configHeaders
+            }
+        }
+    );
+
+    return await response.json();
+};
+
+backendFetch.post = (url, config) => backendFetch(url, { ...config, method: "POST"});
+backendFetch.get = (url, config) => backendFetch(url, { ...config, method: "GET"});
+backendFetch.put = (url, config) => backendFetch(url, { ...config, method: "PUT"});
+backendFetch.delete = (url, config) => backendFetch(url, { ...config, method: "DELETE"});
