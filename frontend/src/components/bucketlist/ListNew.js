224d4641ba2015a7a51c6cdb87fe23aac83e9717
Michael Prasil <michaprx@gmail.com>
2019-07-14 02:17:19 +0200

    Created the form for a new list.

--- frontend/src/components/bucketlist/ListNew.js
+++ frontend/src/components/bucketlist/ListNew.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
 
 class ListNew extends Commponent {
+	state = {
+		title: "",
+		description: ""
+	};
+
+	handleChange(event) {
+		this.setState({ [event.target.name]: event.target.value });
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		backendFetch.post("/bucketlists/add", {
+			body: JSON.stringify({
+				title: this.state.title,
+				description: this.state.description
+			}
+		)}).then(response => {
+			this.props.history.push("/bucketlists/all");
+		});
+	}
+
 	render() {
 		return (
-			<div>
-			</div>
+			<form onSubmit={this.handleSubmit}>
+				<div>
+					<label>Title</label>
+					<div>
+						<input
+							className="input"
+							type="text"
+							name="title"
+							placeholder="Enter Title..."
+							autoFocus={true}
+							value={this.state.title}
+							onChange={this.handleChange}
+							maxLength={1024}
+						/>
+					</div>
+				</div>
+				<div>
+					<label>Description</label>
+					<div>
+						<textarea
+							className="textarea"
+							name="description"
+							value={this.state.description}
+							onChange={this.handleChange}
+							maxLength={4096}
+						/>
+					</div>
+				</div>
+				<div>
+					<div>
+						<input type="submit" value="Submit" />
+					</div>
+					<div>
+						<button>Cancel</button>
+					</div>
+				</div>
+			</form>
 		);
 	}
 }
