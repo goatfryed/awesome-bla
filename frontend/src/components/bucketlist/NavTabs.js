4570a9b84216189934c7686969845c7cc660ca4a
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-24 13:21:46 +0200

    penny for your thoughts

--- frontend/src/components/bucketlist/NavTabs.js
+++ frontend/src/components/bucketlist/NavTabs.js
@@ -2,6 +2,27 @@ import React, {Fragment, useEffect, useRef, useState} from "react";
 import {NavLink, Redirect} from "react-router-dom";
 import * as PropTypes from "prop-types";
 
+/**
+ * react-materialize tabs logic sucks. It does not only style a tab bar a little bit, but is deeply integrated with it's logic
+ * of updating the corresponding tab based on some relation of div id and hash link of the 'li.tab > a'. the logic is not
+ * really suited for rendering tabs on demand, but forces you to render all subtabs beforehand and simply hides the undesired.
+ * also, it's not really suited for integration with react-router.
+ * what we want is to set the active tab by current url and simply update the url, when a tab is clicked. but the default handle
+ * is completely overridden, unless used with target=_self. also, the tabs component doesn't .updateTabIndicator() through active class.
+ * this only works on init of the tabs component.
+ *
+ * we're now overriding the overridden link handler from materialize to set a redirect to the desired route, while using the navlinks
+ * to assess the active link on init.
+ * This gives us the fancy pants tab transition effects. useful? who knows.
+ *
+ * TODO: for some reason, the first transition transitions hard instead of smooth. probably, because materialize asserts the initially active component in some other **** way.
+ * TODO: we're probably better of just using a tab component from another framework  - or styling it ourself... just kidding. Maybe https://bulma.io/documentation/components/tabs/
+ * as single component?
+ *
+ * @param links
+ * @returns {*}
+ * @constructor
+ */
 export function NavTabs({links}) {
 
     const tabsElement = useRef(null);
