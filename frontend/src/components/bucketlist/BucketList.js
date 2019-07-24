11fcbebe620142c622287fb963e1dbeeee7e5e3e
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-07-24 10:55:43 +0200

    tabs with css classes, not working tho

--- frontend/src/components/bucketlist/BucketList.js
+++ frontend/src/components/bucketlist/BucketList.js
@@ -1,10 +1,49 @@
 import React from "react";
 import {BucketListEntries} from "./BucketListEntries";
-import {Link, Redirect} from "react-router-dom";
+import {Link, NavLink, Redirect} from "react-router-dom";
 import {Route, Switch, withRouter} from "react-router";
 import {backendFetch} from "../../api";
 import {CommentsBlock} from "./Comments";
 import moment from "moment";
+import * as PropTypes from "prop-types";
+
+function BucketListDetails(props) {
+    return <article className="media">
+        <figure className="media-left">
+            <p className="image is-64x64">
+                <img src="https://bulma.io/images/placeholders/128x128.png"/>
+            </p>
+        </figure>
+        <div className="media-content">
+            <strong>{props.bucketList.title}({props.bucketList.id})</strong>
+            <br/>
+            <small>{props.counter} · <a onClick={props.onLike}>Like</a>
+                · <span>{moment(props.bucketList.created).fromNow()}</span>
+                · <button onClick={props.onImport}>copy</button>
+            </small>
+        </div>
+    </article>;
+}
+
+BucketListDetails.propTypes = {
+    bucketList: PropTypes.any,
+    counter: PropTypes.number,
+    onLike: PropTypes.func,
+    onImport: PropTypes.func
+};
+
+function SubTabNavigation(props) {
+    return <ul className="tabs">
+        {/* i'd actually prefer to just provide relative routes, but than activeClassName won't match
+                        see https://github.com/ReactTraining/react-router/issues/6201#issuecomment-403291934
+                    */}
+        <li className="tb col s3"><NavLink to={props.url + "/entries"}>Entries</NavLink></li>
+        <li className="tb col s3"><NavLink to={props.url + "/comments"}>Comments</NavLink></li>
+        <li className="tb col s3"><NavLink to={props.url + "/newlistentry"}>New Entry</NavLink></li>
+    </ul>
+}
+
+SubTabNavigation.propTypes = {url: PropTypes.any};
 
 export function BucketList({match, history}) {
     const id = match.params.id;
@@ -51,36 +90,23 @@ export function BucketList({match, history}) {
     }
 
     return <div className="container">
-        <article className="media">
-            <figure className="media-left">
-                <p className="image is-64x64">
-                    <img src="https://bulma.io/images/placeholders/128x128.png" />
-                </p>
-            </figure>
-            <div className="media-content">
-                <strong>{bucketList.title}({bucketList.id})</strong>
-                <br/>
-                <small>{counter} · <a onClick={incrementCounter}>Like</a>
-                    · <span>{moment(bucketList.created).fromNow()}</span>
-                    · <button onClick={importBucketList}>copy</button>
-                </small>
-            </div>
-        </article>
-        <div className="tabs">
-            <ul>
-                {/* i'd actually prefer to just provide relative routes, but than activeClassName won't match
-                    see https://github.com/ReactTraining/react-router/issues/6201#issuecomment-403291934
-                */}
-                <NavTab to={match.url+"/entries"}>Entries</NavTab>
-                <NavTab to={match.url+"/comments"}>Comments</NavTab>
-                <NavTab to={match.url+"/newlistentry"}>New Entry</NavTab>
-            </ul>
+        <div className="row">
+            <BucketListDetails bucketList={bucketList} counter={counter} onLike={incrementCounter}
+                               onImport={importBucketList}/>
         </div>
-        <Switch>
-            <Route path={match.path+"entries"}  render={() => <BucketListEntries id={id}/>} />
-            <Route path={match.path+"comments"} render={() => <BucketListComments bucketList={bucketList} update={update}/>} />
-            <Redirect to={match.url+"/entries"} />
-        </Switch>
+        <div className="row">
+            <SubTabNavigation url={match.url}/>
+        </div>
+        <div className="row">
+            <Switch>
+                <Route path={match.path + "entries"}
+                       render={() => <div className="col"><BucketListEntries id={id}/></div>}/>
+                <Route path={match.path + "comments"}
+                       render={() => <div className="col"><BucketListComments bucketList={bucketList} update={update}/>
+                       </div>}/>
+                <Redirect to={match.url + "/entries"}/>
+            </Switch>
+        </div>
     </div>
 }
 
