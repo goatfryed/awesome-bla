2d43d18b306576fe3021253cd573f4677b769e55
tost <tost@tost-soft.de>
2019-08-06 17:39:25 +0200

    fixed cross origine

--- backend/src/main/java/de/uniks/webengineering2019/bla/controllers/UserController.java
+++ backend/src/main/java/de/uniks/webengineering2019/bla/controllers/UserController.java
@@ -7,15 +7,13 @@ import de.uniks.webengineering2019.bla.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin
 public class UserController{
 
     @Autowired
