f91abd05508beb2337f89b12df74426ff141f520
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-09-04 13:09:57 +0200

    fixed backend url for heroku deploy

--- deploy/heroku/frontend.sh
+++ deploy/heroku/frontend.sh
@@ -3,5 +3,5 @@
 cd "$(dirname "$0")"
 echo "Deploying the frontend"
 cd ../../frontend
-heroku container:push web -a awesome-bla
+heroku container:push web -a awesome-bla --arg BACKEND_URL="https://awesome-bla-api.herokuapp.com"
 heroku container:release web -a awesome-bla
