e95379b1a4a3146048023cb9edc6da88d828515c
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-09-05 16:33:48 +0200

    allowed override of link behaviour of all primary links of a bucket list in bucketlist listview

--- frontend/src/components/bucketlist/BucketListBoard.js
+++ frontend/src/components/bucketlist/BucketListBoard.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, useCallback } from 'react';
 import {Route, Switch} from "react-router";
 import { Link, Redirect } from "react-router-dom";
 import { backendFetch } from "../../api";
@@ -116,17 +116,28 @@ function BucketListIcon({bucketList}) {
 	return <i title={iconDescriptor.title} className={"material-icons circle "+iconDescriptor.class} >{iconDescriptor.icon}</i>;
 }
 
-function BucketListEntry({bucketList}) {
+// use onInteraction to override behaviour of all links to the bucket lists
+// is used in importer to trigger an import action instead of routing
+function BucketListEntry({bucketList, onInteraction}) {
 
+    let onMainInteraction = useCallback(
+        e => {
+            if (!onInteraction) return;
+            e.stopPropagation();
+            e.preventDefault();
+
+            onInteraction(bucketList);
+        },
+        [bucketList, onInteraction]
+    );
 
 
 	return <li className="collection-item avatar" style={{minHeight: "initial"}}>
-		<Link to={"/bucketlist/" + bucketList.id}
-			  onClick={e => {e.stopPropagation(); e.preventDefault(); console.log(e)}}
+		<Link to={"/bucketlist/" + bucketList.id} onClick={onMainInteraction}
 		><BucketListIcon bucketList={bucketList}/></Link>
 		<div className="row" style={{marginBottom: "initial"}}>
 			<div className="col">
-				<span className="title"><Link to={"/bucketlist/" + bucketList.id}>{bucketList.title}</Link></span>
+				<span className="title"><Link to={"/bucketlist/" + bucketList.id} onClick={onMainInteraction}>{bucketList.title}</Link></span>
 				<p>{
 					bucketList.ownList ? <span>your List</span>
 						: <span>{bucketList.privateList ? "shared by" : "by"} {bucketList.owner.userName}</span>
@@ -136,15 +147,17 @@ function BucketListEntry({bucketList}) {
 				<span>{bucketList.description}</span>
 			</div>
 		</div>
-		<Link to={"/bucketlist/" + bucketList.id} className="secondary-content"><i
+		<Link to={"/bucketlist/" + bucketList.id}
+              onClick={onMainInteraction}
+              className="secondary-content"><i
 			className="material-icons">send</i></Link>
 	</li>;
 }
 
-const Lists = ({ bucketLists }) => {
+const Lists = ({ bucketLists, onListInteraction }) => {
 	return <ul className="collection lighten-1">
 		{
-			bucketLists.map(bucketList => <BucketListEntry  key={bucketList.id}  bucketList={bucketList}/>)
+			bucketLists.map(bucketList => <BucketListEntry  key={bucketList.id}  bucketList={bucketList} onInteraction={onListInteraction} />)
 		}
 	</ul>
 };
