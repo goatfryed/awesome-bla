c385c62a2d353b05ea04bea6a25567d92e16bece
Omar Goatfryed Sood <goatfryed@gmail.com>
2019-10-25 12:15:30 +0200

    fix issue with multiple results from join with accessed users

--- backend/src/main/java/de/uniks/webengineering2019/bla/repositories/BucketListRepository.java
+++ backend/src/main/java/de/uniks/webengineering2019/bla/repositories/BucketListRepository.java
@@ -18,7 +18,7 @@ public interface BucketListRepository extends CrudRepository<BucketList, Long>{
     //Set<BucketList> findByOwnerAndTitleContainsIgnoreCaseOrderByCreationDateDescIdDesc(User owner, String title);
 
 
-    @Query(value = "SELECT b.* FROM bucket_list AS b LEFT JOIN bucket_list_accessed_users AS al ON b.id = al.bucket_list_id WHERE lower(b.title) LIKE ?2 AND ( b.private_list = FALSE OR b.owner_id = ?1 OR al.accessed_users_id = ?1 ) ORDER BY b.creation_date desc, b.id desc;",nativeQuery = true)
+    @Query(value = "SELECT b.* FROM bucket_list AS b LEFT JOIN bucket_list_accessed_users AS al ON b.id = al.bucket_list_id WHERE lower(b.title) LIKE ?2 AND ( b.private_list = FALSE OR b.owner_id = ?1 OR al.accessed_users_id = ?1 ) GROUP BY b.id ORDER BY b.id desc;",nativeQuery = true)
     List<BucketList> findByNameAndPrivelege(long userId,String searchTerm);
     List<BucketList> findByPrivateListAndTitleContainingIgnoreCaseOrderByCreationDateDescIdDesc(boolean privateList,String searchTerm);
 
@@ -33,6 +33,6 @@ public interface BucketListRepository extends CrudRepository<BucketList, Long>{
 
     Page<BucketList> findByOwnerIdAndPrivateList(long userId,boolean privateList,Pageable pageable);
 
-    @Query(value = "SELECT b FROM bucket_list AS b LEFT JOIN bucket_list_accessed_users AS al ON b.id = al.bucket_list_id WHERE b.owner_id = ?1 AND ( b.private_list = FALSE OR b.owner_id = ?2 OR al.accessed_users_id = ?2 ) ORDER BY  b.creation_date desc, b.id desc;",nativeQuery = true)
+    @Query(value = "SELECT b FROM bucket_list AS b LEFT JOIN bucket_list_accessed_users AS al ON b.id = al.bucket_list_id WHERE b.owner_id = ?1 AND ( b.private_list = FALSE OR b.owner_id = ?2 OR al.accessed_users_id = ?2 ) GROUP BY b.id ORDER BY b.id desc;",nativeQuery = true)
     Page<BucketList> findByownerIdandPriveleged(long ownerId,long userId,Pageable pageable);
 }
